8cdd96e35cd6b7aca3febad5b5a2a0f1fb2101b6
stanislaw <sas.tomczak@wp.pl>
2025-12-17 11:54:40 +0100

    initial commit, metody w klasie SavingsGoals

---
+++ src/BudgetObserver.java
@@ -0,0 +1,10 @@
+public interface BudgetObserver {
+    public void update(double totalExpenses, double totalIncome);
+}
+/*
+Interfejs BudgetObserver
+
+Metody:
+
+update(totalExpenses, totalIncome) – Metoda wywoływana, gdy BudgetManager zgłosi zmianę danych
+ */
---
+++ src/SystemLogger.java
@@ -0,0 +1,14 @@
+public class SystemLogger implements BudgetObserver{
+    public void update(double totalExpenses, double totalIncome){
+
+    }
+}
+/*
+Klasa SystemLogger
+
+Techniczny obserwator.
+
+Metody:
+
++ update(...) – Zapisuje logi systemowe (np. "System zaktualizowany o 12:00. Nowe saldo: 500zł").
+*/
